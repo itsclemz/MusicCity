25070ea963428dc18fb3927de59eb7fcb2abc474
Isaac Clement <isaacclem7@gmail.com>
2020-05-13 19:06:51 +0100

    Add project files.

---
+++ MusicCity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicCity.Startup))]
+namespace MusicCity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
